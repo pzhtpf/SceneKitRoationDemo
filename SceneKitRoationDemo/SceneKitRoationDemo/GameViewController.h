9dcd99b1fb75241197b0a29a50fee6f64996f804
tianpengfei <389744841@qq.com>
2016-05-05 18:11:00 +0800

    first commit

---
+++ SceneKitRoationDemo/SceneKitRoationDemo/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  SceneKitRoationDemo
+//
+
+//  Copyright (c) 2016年 tianpengfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
---
+++ SceneKitRoationDemo/SceneKitRoationDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SceneKitRoationDemo
+//
+//  Created by tianpengfei on 16/5/5.
+//  Copyright © 2016年 tianpengfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
