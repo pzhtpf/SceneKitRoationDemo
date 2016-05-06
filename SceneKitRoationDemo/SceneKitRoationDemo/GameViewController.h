090c73bcd33eeec56f7c284441c8cbc849406b80
tianpengfei <389744841@qq.com>
2016-05-06 22:02:03 +0800

    add function

--- SceneKitRoationDemo/SceneKitRoationDemo/GameViewController.h
+++ SceneKitRoationDemo/SceneKitRoationDemo/GameViewController.h
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import <SceneKit/SceneKit.h>
+#import <QuartzCore/QuartzCore.h>
 
 @interface GameViewController : UIViewController
 
--- SceneKitRoationDemo/SceneKitRoationDemo/GameViewController.m
+++ SceneKitRoationDemo/SceneKitRoationDemo/GameViewController.m
@@ -8,6 +8,13 @@
 
 #import "GameViewController.h"
 
+@interface GameViewController ()
+
+@property(strong,nonatomic)SCNNode *sunNode,*earthNode,*moonNode,*earthGroupNode;
+@property(strong,nonatomic)SCNView *scnView ;
+
+@end
+
 @implementation GameViewController
 
 - (void)viewDidLoad
@@ -25,52 +32,189 @@
     // place the camera
     cameraNode.position = SCNVector3Make(0, 0, 15);
     
-    // create and add a light to the scene
-    SCNNode *lightNode = [SCNNode node];
-    lightNode.light = [SCNLight light];
-    lightNode.light.type = SCNLightTypeOmni;
-    lightNode.position = SCNVector3Make(0, 10, 10);
-    [scene.rootNode addChildNode:lightNode];
-    
-    // create and add an ambient light to the scene
-    SCNNode *ambientLightNode = [SCNNode node];
-    ambientLightNode.light = [SCNLight light];
-    ambientLightNode.light.type = SCNLightTypeAmbient;
-    ambientLightNode.light.color = [UIColor darkGrayColor];
-    [scene.rootNode addChildNode:ambientLightNode];
+//    // create and add a light to the scene
+//    SCNNode *lightNode = [SCNNode node];
+//    lightNode.light = [SCNLight light];
+//    lightNode.light.type = SCNLightTypeOmni;
+//    lightNode.position = SCNVector3Make(0, 10, 10);
+//    [scene.rootNode addChildNode:lightNode];
+//    
+//    // create and add an ambient light to the scene
+//    SCNNode *ambientLightNode = [SCNNode node];
+//    ambientLightNode.light = [SCNLight light];
+//    ambientLightNode.light.type = SCNLightTypeAmbient;
+//    ambientLightNode.light.color = [UIColor darkGrayColor];
+//    [scene.rootNode addChildNode:ambientLightNode];
     
     // retrieve the ship node
     SCNNode *ship = [scene.rootNode childNodeWithName:@"ship" recursively:YES];
+    [ship setHidden:YES];
     
     // animate the 3d object
     [ship runAction:[SCNAction repeatActionForever:[SCNAction rotateByX:0 y:2 z:0 duration:1]]];
     
     // retrieve the SCNView
-    SCNView *scnView = (SCNView *)self.view;
+    _scnView = (SCNView *)self.view;
     
     // set the scene to the view
-    scnView.scene = scene;
+    _scnView.scene = scene;
     
     // allows the user to manipulate the camera
-    scnView.allowsCameraControl = YES;
+    _scnView.allowsCameraControl = YES;
         
     // show statistics such as fps and timing information
-    scnView.showsStatistics = YES;
+    _scnView.showsStatistics = YES;
 
     // configure the view
-    scnView.backgroundColor = [UIColor blackColor];
+    _scnView.backgroundColor = [UIColor blackColor];
     
     // add a tap gesture recognizer
     UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTap:)];
     NSMutableArray *gestureRecognizers = [NSMutableArray array];
     [gestureRecognizers addObject:tapGesture];
-    [gestureRecognizers addObjectsFromArray:scnView.gestureRecognizers];
-    scnView.gestureRecognizers = gestureRecognizers;
+    [gestureRecognizers addObjectsFromArray:_scnView.gestureRecognizers];
+    _scnView.gestureRecognizers = gestureRecognizers;
+    
+    
+    [self initNode];
 }
 
 -(void)initNode{
 
+    _sunNode = [SCNNode new];
+    _earthNode = [SCNNode new];
+    _moonNode = [SCNNode new];
+    _earthGroupNode = [SCNNode new];
     
+    _sunNode.geometry = [SCNSphere sphereWithRadius:2.5];
+    _earthNode.geometry = [SCNSphere sphereWithRadius:1.5];
+    _moonNode.geometry = [SCNSphere sphereWithRadius:0.5];
+    
+    _moonNode.position = SCNVector3Make(3, 0, 0);
+//    [_earthGroupNode addChildNode:_moonNode];
+    [_earthGroupNode addChildNode:_earthNode];
+    
+    _earthGroupNode.position = SCNVector3Make(10, 0, 0);
+    
+//    [_sunNode addChildNode:_earthGroupNode];
+    
+    [_scnView.scene.rootNode addChildNode:_sunNode];
+
+    
+    
+    // Add materials to the planets
+    _earthNode.geometry.firstMaterial.diffuse.contents = @"art.scnassets/earth/earth-diffuse-mini.jpg";
+    _earthNode.geometry.firstMaterial.emission.contents = @"art.scnassets/earth/earth-emissive-mini.jpg";
+    _earthNode.geometry.firstMaterial.specular.contents = @"art.scnassets/earth/earth-specular-mini.jpg";
+    _moonNode.geometry.firstMaterial.diffuse.contents = @"art.scnassets/earth/moon.jpg";
+    _sunNode.geometry.firstMaterial.multiply.contents = @"art.scnassets/earth/sun.jpg";
+    _sunNode.geometry.firstMaterial.diffuse.contents = @"art.scnassets/earth/sun.jpg";
+    _sunNode.geometry.firstMaterial.multiply.intensity = 0.5;
+    _sunNode.geometry.firstMaterial.lightingModelName = SCNLightingModelConstant;
+    
+    _sunNode.geometry.firstMaterial.multiply.wrapS =
+    _sunNode.geometry.firstMaterial.diffuse.wrapS  =
+    _sunNode.geometry.firstMaterial.multiply.wrapT =
+    _sunNode.geometry.firstMaterial.diffuse.wrapT  = SCNWrapModeRepeat;
+    
+    _earthNode.geometry.firstMaterial.locksAmbientWithDiffuse =
+    _moonNode.geometry.firstMaterial.locksAmbientWithDiffuse  =
+    _sunNode.geometry.firstMaterial.locksAmbientWithDiffuse   = YES;
+    
+    _earthNode.geometry.firstMaterial.shininess = 0.1;
+    _earthNode.geometry.firstMaterial.specular.intensity = 0.5;
+    _moonNode.geometry.firstMaterial.specular.contents = [UIColor grayColor];
+    
+    
+    [self roationNode];
+    [self addLight];
+}
+-(void)roationNode{
+
+    [_earthNode runAction:[SCNAction repeatActionForever:[SCNAction rotateByX:0 y:2 z:0 duration:1]]];   //地球自转
+
+    
+    // Earth-rotation (center of rotation of the Earth around the Sun)
+    SCNNode *earthRotationNode = [SCNNode node];
+    [_sunNode addChildNode:earthRotationNode];
+    
+    // Earth-group (will contain the Earth, and the Moon)
+    [earthRotationNode addChildNode:_earthGroupNode];
+    
+    // Rotate the Earth around the Sun
+    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"rotation"];
+    animation.duration = 10.0;
+    animation.toValue = [NSValue valueWithSCNVector4:SCNVector4Make(0, 1, 0, M_PI * 2)];
+    animation.repeatCount = FLT_MAX;
+    [earthRotationNode addAnimation:animation forKey:@"earth rotation around sun"];
+    
+    
+    
+    // Moon-rotation (center of rotation of the Moon around the Earth)
+    SCNNode *moonRotationNode = [SCNNode node];
+    
+    [moonRotationNode addChildNode:_moonNode];
+    
+    // Rotate the moon around the Earth
+    CABasicAnimation *moonRotationAnimation = [CABasicAnimation animationWithKeyPath:@"rotation"];
+    moonRotationAnimation.duration = 1.5;
+    moonRotationAnimation.toValue = [NSValue valueWithSCNVector4:SCNVector4Make(0, 1, 0, M_PI * 2)];
+    moonRotationAnimation.repeatCount = FLT_MAX;
+    [moonRotationNode addAnimation:animation forKey:@"moon rotation around earth"];
+    
+    [_earthGroupNode addChildNode:moonRotationNode];
+    
+    // Rotate the moon
+    animation = [CABasicAnimation animationWithKeyPath:@"rotation"];
+    animation.duration = 1.5;
+    animation.toValue = [NSValue valueWithSCNVector4:SCNVector4Make(0, 1, 0, M_PI * 2)];
+    animation.repeatCount = FLT_MAX;
+    [_moonNode addAnimation:animation forKey:@"moon rotation"];
+    
+    [self addAnimationToSun];
+}
+-(void)addAnimationToSun{
+
+    // Achieve a lava effect by animating textures
+    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"contentsTransform"];
+    animation.duration = 10.0;
+    animation.fromValue = [NSValue valueWithCATransform3D:CATransform3DConcat(CATransform3DMakeTranslation(0, 0, 0), CATransform3DMakeScale(3, 3, 3))];
+    animation.toValue = [NSValue valueWithCATransform3D:CATransform3DConcat(CATransform3DMakeTranslation(1, 0, 0), CATransform3DMakeScale(3, 3, 3))];
+    animation.repeatCount = FLT_MAX;
+    [_sunNode.geometry.firstMaterial.diffuse addAnimation:animation forKey:@"sun-texture"];
+    
+    animation = [CABasicAnimation animationWithKeyPath:@"contentsTransform"];
+    animation.duration = 30.0;
+    animation.fromValue = [NSValue valueWithCATransform3D:CATransform3DConcat(CATransform3DMakeTranslation(0, 0, 0), CATransform3DMakeScale(5, 5, 5))];
+    animation.toValue = [NSValue valueWithCATransform3D:CATransform3DConcat(CATransform3DMakeTranslation(1, 0, 0), CATransform3DMakeScale(5, 5, 5))];
+    animation.repeatCount = FLT_MAX;
+    [_sunNode.geometry.firstMaterial.multiply addAnimation:animation forKey:@"sun-texture2"];
+
+}
+-(void)addLight{
+
+    // We will turn off all the lights in the scene and add a new light
+    // to give the impression that the Sun lights the scene
+    SCNNode *lightNode = [SCNNode node];
+    lightNode.light = [SCNLight light];
+    lightNode.light.color = [UIColor blackColor]; // initially switched off
+    lightNode.light.type = SCNLightTypeOmni;
+    [_sunNode addChildNode:lightNode];
+    
+    // Configure attenuation distances because we don't want to light the floor
+    lightNode.light.attenuationEndDistance = 20.0;
+    lightNode.light.attenuationStartDistance = 19.5;
+    
+    // Animation
+    [SCNTransaction begin];
+    [SCNTransaction setAnimationDuration:1];
+    {
+      
+        lightNode.light.color = [UIColor whiteColor]; // switch on
+        //[presentationViewController updateLightingWithIntensities:@[@0.0]]; //switch off all the other lights
+//        _sunHaloNode.opacity = 0.5; // make the halo stronger
+    }
+    [SCNTransaction commit];
 
 
 }
